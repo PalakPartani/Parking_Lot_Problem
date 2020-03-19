8917c56f243fb676369ce7443e04559e20238bdb
PalakPartani <partanipalak16@gmail.com>
2020-03-19 08:55:57 -0700

    ADDED:testcase to check the initial capacity of parking lot

--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -4,4 +4,9 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class ParkingLotTest {
-}
+    @Test
+    public void givenParkingLotCapacity100_WhenCorrect_ShouldReturnTrue() {
+        ParkingLot parkingLotSystem = new ParkingLot();
+        int capacity = parkingLotSystem.getParkingCapacity();
+        Assert.assertEquals(100,capacity);
+    }}
