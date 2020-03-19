f81fd28725dc0bffa77d3f39738af62140d0abea
PalakPartani <partanipalak16@gmail.com>
2020-03-19 08:53:49 -0700

    ADDED:project with base files

---
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -0,0 +1,9 @@
+package com.parkinglot;
+
+public class ParkingLot {
+    private int[] parkingCapacity=new int[100];
+    public int getParkingCapacity() {
+        return parkingCapacity.length;
+    }
+}
+
---
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -0,0 +1,7 @@
+package com.parkinglot;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ParkingLotTest {
+}
